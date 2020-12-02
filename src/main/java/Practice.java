d3d290c14a4056fb944150d63c2b6e91b5ed5f31
Jacob Huang <jacob.huang@gogoro.com>
2020-12-02 16:01:10 +0800

    print parenthesis

--- src/main/java/Practice.java
+++ src/main/java/Practice.java
@@ -1,6 +1,10 @@
-public class Practice {
-    public static void main() {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
+public class Practice {
+    public static void main(String[] args) {
+        generateParenthesis(3).stream().forEach(x -> System.out.print(x));
     }
     /**
      * Definition for singly-linked list.
@@ -67,6 +71,36 @@ public class Practice {
             }
             return copyRandomList(head);
         }
+    }
+
+    public static List<String> generateParenthesis (int n) {
+        List<String> result = new ArrayList<>();
+        Stack<String> stack = new Stack<>();
+        for (int i = 1; i <= n; i++) {
+            int j = 0;
+            while (j<i) {
+                int k = j;
+                for (; k<i; k++){
+                    stack.push("(");
+                }
+                for (; k>0; k--) {
+                    stack.push(")");
+                }
+                j++;
+            }
+            while(j<n){
+                int z = j;
+                for (; z<n; z++){
+                    stack.push("(");
+                }
+                for (; z>j; z--) {
+                    stack.push(")");
+                }
+                j++;
+            }
+        }
+        stack.forEach(x-> result.add(x));
+        return result;
     }
 
 }
