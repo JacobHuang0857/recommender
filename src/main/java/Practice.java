c3c941186ac3df2bb7a7089e9d0810baedbf8de1
Jacob Huang <jacob.huang@gogoro.com>
2020-12-04 17:50:36 +0800

    Rotate Matrix sample

--- src/main/java/Practice.java
+++ src/main/java/Practice.java
@@ -356,4 +356,20 @@ public class Practice {
             a[k++] = r[j++];
         }
     }
+
+    public void rotate(int[][] matrix) {
+
+        int size = matrix.length;
+        for(int i = 0 ; i < (size + 1) / 2 ; i++){
+            for(int j = 0 ; j < size / 2 ; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[size - j - 1][i];
+                matrix[size - j - 1][i] = matrix[size - i - 1][size - j - 1];
+                matrix[size - i - 1][size - j - 1] = matrix[j][size - i - 1];
+                matrix[j][size - i - 1] = temp;
+
+            }
+        }
+
+    }
 }
