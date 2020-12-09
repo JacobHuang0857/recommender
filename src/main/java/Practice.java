76748002fca7d3715f277683bf6172acd2efdec1
Jacob Huang <jacob.huang@gogoro.com>
2020-12-09 11:05:57 +0800

    retrieve BST inorder.

--- src/main/java/Practice.java
+++ src/main/java/Practice.java
@@ -382,4 +382,31 @@ public class Practice {
         }
         return map.values().stream().max(Integer::compare).get();
     }
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> results = new ArrayList<>();
+        if (root != null) {
+            results.addAll(inorderTraversal(root.left));
+            results.add(root.value);
+            results.addAll(inorderTraversal(root.right));
+        }
+        return results;
+    }
+
 }
